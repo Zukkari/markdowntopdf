1f3c9807b6c0171da46250d133074d6fcc4d665b
Zukkari <stanislav.myshkovski@gmail.com>
2019-03-07 21:48:05 +0200

    delete client library since its not needed anymore

--- pdf-stats-client/src/main/java/com/github/zukkari/stats/client/exception/ConnectionFailure.java
+++
@@ -1,8 +0,0 @@
-package com.github.zukkari.stats.client.exception;
-
-public class ConnectionFailure extends RuntimeException {
-
-    public ConnectionFailure(Exception e) {
-        super(e);
-    }
-}
--- pdf-stats-client/src/main/java/com/github/zukkari/stats/client/exception/InvalidRequestException.java
+++
@@ -1,8 +0,0 @@
-package com.github.zukkari.stats.client.exception;
-
-public class InvalidRequestException extends RuntimeException {
-
-    public InvalidRequestException(int code) {
-        super(String.format("Response code is not OK: %d", code));
-    }
-}
