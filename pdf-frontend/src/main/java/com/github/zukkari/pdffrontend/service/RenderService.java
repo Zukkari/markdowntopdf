7893c9e58e45c62d1d6d6a7e3fc38ab4b27d9718
Zukkari <stanislav.myshkovski@gmail.com>
2019-03-09 16:11:59 +0200

    remove external js/css and replace it with cdn

--- pdf-frontend/src/main/java/com/github/zukkari/pdffrontend/service/RenderService.java
+++ pdf-frontend/src/main/java/com/github/zukkari/pdffrontend/service/RenderService.java
@@ -12,8 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.InputStream;
-import java.net.URI;
 import java.net.URISyntaxException;
 
 @Service
